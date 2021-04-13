750525d4c65e5bc1ecbfcbbab9df54eeee74277f
小羊 <tlylz99@live.cn>
2021-04-13 22:02:52 +0800

    Add IP range detect for IPv4.

--- src/Abstraction/Services/IpMatcher.cs
+++ src/Abstraction/Services/IpMatcher.cs
@@ -1,4 +1,6 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Ccs.Services
 {
@@ -7,12 +9,30 @@ namespace Ccs.Services
     /// </summary>
     public static class IpMatcher
     {
+        private static uint ToUint32(this IPAddress address)
+        {
+            var toCheckBytes = address.GetAddressBytes();
+            if (BitConverter.IsLittleEndian) Array.Reverse(toCheckBytes);
+            return BitConverter.ToUInt32(toCheckBytes, 0);
+        }
+
         /// <summary>
         /// Checks whether the IP and subnet matches the client IP.
         /// </summary>
         public static bool IsInRange(this IPAddress toCheck, IPAddress range, int subnet)
         {
-            return false;
+            if (toCheck.AddressFamily == AddressFamily.InterNetwork
+                && range.AddressFamily == AddressFamily.InterNetwork)
+            {
+                var toCheckUint = toCheck.ToUint32();
+                var rangeUint = range.ToUint32();
+                var maskUint = ((ulong.MaxValue & uint.MaxValue) >> (32 - subnet)) << (32 - subnet);
+                return (toCheckUint & maskUint) == (rangeUint & maskUint);
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
---
+++ test/Infrastructure/IpMatching.cs
@@ -0,0 +1,24 @@
+﻿using Ccs.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+
+namespace Ccs.Tests.Infrastructure
+{
+    [TestClass]
+    public class IpMatching
+    {
+        [DataTestMethod]
+        [DataRow("192.168.1.233", "192.168.1.0", 24, true)]
+        [DataRow("192.168.1.233", "192.168.2.0", 24, false)]
+        [DataRow("192.168.3.233", "192.168.2.0", 23, true)]
+        [DataRow("192.168.3.233", "192.168.3.233", 32, true)]
+        [DataRow("192.168.3.233", "0.0.0.0", 0, true)]
+        [DataRow("192.168.3.233", "0.0.0.0", 32, false)]
+        public void EnsureMatchResult(string client, string range, int subnet, bool result)
+        {
+            var clientIp = IPAddress.Parse(client);
+            var rangeIp = IPAddress.Parse(range);
+            Assert.AreEqual(result, clientIp.IsInRange(rangeIp, subnet));
+        }
+    }
+}
