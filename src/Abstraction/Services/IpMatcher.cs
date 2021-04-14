f75902e3b94c6d5ed6b540126ac97e2c17e049aa
小羊 <tlylz99@live.cn>
2021-04-14 11:16:50 +0800

    Add 4to6 and v6 support.

--- src/Abstraction/Services/IpMatcher.cs
+++ src/Abstraction/Services/IpMatcher.cs
@@ -9,11 +9,18 @@ namespace Ccs.Services
     /// </summary>
     public static class IpMatcher
     {
-        private static uint ToUint32(this IPAddress address)
+        private static uint ToUInt32(this IPAddress address)
         {
             var toCheckBytes = address.GetAddressBytes();
-            if (BitConverter.IsLittleEndian) Array.Reverse(toCheckBytes);
-            return BitConverter.ToUInt32(toCheckBytes, 0);
+            return (uint)IPAddress.NetworkToHostOrder(BitConverter.ToInt32(toCheckBytes, 0));
+        }
+
+        private static (ulong hi, ulong lo) ToUInt64s(this IPAddress address)
+        {
+            var toCheckBytes = address.GetAddressBytes();
+            var hi = (ulong)IPAddress.NetworkToHostOrder(BitConverter.ToInt64(toCheckBytes, 0));
+            var lo = (ulong)IPAddress.NetworkToHostOrder(BitConverter.ToInt64(toCheckBytes, 8));
+            return (hi, lo);
         }
 
         /// <summary>
@@ -21,13 +28,45 @@ namespace Ccs.Services
         /// </summary>
         public static bool IsInRange(this IPAddress toCheck, IPAddress range, int subnet)
         {
-            if (toCheck.AddressFamily == AddressFamily.InterNetwork
-                && range.AddressFamily == AddressFamily.InterNetwork)
+            if (toCheck.IsIPv4MappedToIPv6) toCheck = toCheck.MapToIPv4();
+
+            if ((toCheck.AddressFamily != AddressFamily.InterNetwork
+                && toCheck.AddressFamily != AddressFamily.InterNetworkV6)
+                || (range.AddressFamily != AddressFamily.InterNetwork
+                && range.AddressFamily != AddressFamily.InterNetworkV6))
+                throw new NotImplementedException();
+
+            if (toCheck.AddressFamily != range.AddressFamily)
             {
-                var toCheckUint = toCheck.ToUint32();
-                var rangeUint = range.ToUint32();
-                var maskUint = ((ulong.MaxValue & uint.MaxValue) >> (32 - subnet)) << (32 - subnet);
+                return false;
+            }
+            if (toCheck.AddressFamily == AddressFamily.InterNetwork)
+            {
+                var toCheckUint = toCheck.ToUInt32();
+                var rangeUint = range.ToUInt32();
+                var maskUint = (uint)(((1L << subnet) - 1) << (32 - subnet));
                 return (toCheckUint & maskUint) == (rangeUint & maskUint);
+            }
+            else if (toCheck.AddressFamily == AddressFamily.InterNetworkV6)
+            {
+                var (tchi, tclo) = toCheck.ToUInt64s();
+                var (rghi, rglo) = range.ToUInt64s();
+
+                if (subnet == 64)
+                {
+                    return tchi == rghi;
+                }
+                else if (subnet < 64)
+                {
+                    // tclo and rglo is ignored
+                    var mask = (ulong)(((1L << subnet) - 1) << (64 - subnet));
+                    return (tchi & mask) == (rghi & mask);
+                }
+                else
+                {
+                    var mask = (ulong)(((1L << (subnet - 64)) - 1) << (128 - subnet));
+                    return tchi == rghi && (tclo & mask) == (rglo & mask);
+                }
             }
             else
             {
--- test/Infrastructure/IpMatching.cs
+++ test/Infrastructure/IpMatching.cs
@@ -14,6 +14,18 @@ namespace Ccs.Tests.Infrastructure
         [DataRow("192.168.3.233", "192.168.3.233", 32, true)]
         [DataRow("192.168.3.233", "0.0.0.0", 0, true)]
         [DataRow("192.168.3.233", "0.0.0.0", 32, false)]
+        [DataRow("::ffff:111.26.82.49", "111.26.82.0", 24, true)]
+        [DataRow("::ffff:111.26.82.49", "111.26.82.0", 31, false)]
+        [DataRow("2001:da8:b000:123:4567:89ab:cdef::", "2001:da8:b000::", 48, true)]
+        [DataRow("192.168.3.233", "2001:da8:b000::", 48, false)]
+        [DataRow("::ffff:111.26.82.49", "2001:da8:b000::", 48, false)]
+        [DataRow("2001:db8:b000:123:4567:89ab:cdef::", "2001:da8:b000::", 48, false)]
+        [DataRow("2001:da8:b000::", "2001:da8:b000::", 128, true)]
+        [DataRow("2001:da8:b000::", "::", 0, true)]
+        [DataRow("2001:da8:b000:123:4567:89ab:cdef::", "2001:da8:b000:123::", 64, true)]
+        [DataRow("2001:da8:b000:123:4567:89ab:cdef::", "2001:da8:b000:123::", 65, true)]
+        [DataRow("2001:da8:b000:123:4567:89ab:cdef::", "2001:da8:b000:123::", 66, false)]
+        [DataRow("2001:da8:b000:123:4567:89ab:cdef::", "2001:db8:b000:123::", 65, false)]
         public void EnsureMatchResult(string client, string range, int subnet, bool result)
         {
             var clientIp = IPAddress.Parse(client);
